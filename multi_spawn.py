35aba726a81c7c04205c2ae484ba9c298b95d280
Istvan Hoffer <hofferistvan@me.com>
2018-03-01 19:04:46 +0000

    adding multi spawn script

---
+++ multi_spawn.py
@@ -0,0 +1,22 @@
+import argparse
+import subprocess
+
+#processing arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-n', metavar='i', default=50, type=int, help='Number of processes to spawn')
+args = parser.parse_args()
+
+numProcess = args.n
+
+bash = ""
+for i in range(numProcess - 1):
+    bash += "node wallet.js --debug > log{}.txt &\n".format(i)
+
+bash += "node wallet.js --debug > log{}.txt\n".format(numProcess - 1) #final line shouldn't have & at end
+
+with open("spawn.sh", "w") as file:
+    file.write(bash) #writing that list of commands to a file
+
+subprocess.run(["chmod", "+x", "spawn.sh"]) #make that file executable
+
+subprocess.call("./spawn.sh", shell=True) #run and wait
