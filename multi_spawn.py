e01d41dbf5da6ac93aea163e66639e026ba1acb0
Istvan Hoffer <hofferistvan@me.com>
2018-03-01 19:45:41 +0000

    some refactoring

--- multi_spawn.py
+++ multi_spawn.py
@@ -6,17 +6,19 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-n', metavar='i', default=50, type=int, help='Number of processes to spawn')
 args = parser.parse_args()
 
+command = "node wallet.js --debug > log{}.txt"
+
 numProcess = args.n
 
 bash = ""
 for i in range(numProcess - 1):
-    bash += "node wallet.js --debug > log{}.txt &\n".format(i)
+    bash += "{} &\n".format(command.format(i))
 
-bash += "node wallet.js --debug > log{}.txt\n".format(numProcess - 1) #final line shouldn't have & at end
+bash += command.format(numProcess - 1) #final line shouldn't have & at end
 
 with open("spawn.sh", "w") as file:
     file.write(bash) #writing that list of commands to a file
 
 subprocess.run(["chmod", "+x", "spawn.sh"]) #make that file executable
 
-subprocess.call("./spawn.sh", shell=True) #run and wait
+#subprocess.call("./spawn.sh", shell=True) #run and wait
